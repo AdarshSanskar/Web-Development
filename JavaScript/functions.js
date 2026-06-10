82a29124ab6201cad2da67c39ff2d00bbff4ec9c
AdarshSanskar <astile1424@gmail.com>
2026-06-10 15:13:13 +0530

    Update functions.js

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -17,5 +17,34 @@ function orderTea(teaType) {
 let orderConfirmation = orderTea("Green Tea");
 // console.log(orderConfirmation);
 
-
 // 3. Write an arrow function named 'calculateTotal' that takes two parameters: 'price' and 'quantity'. The function should return the total cost by multiplying the 'price' and 'quantity'. Store the result in a variable named 'totalCost'.
+
+const calculateTotal = (price, quantity) => {
+  return price * quantity;
+};
+
+let totalCost = calculateTotal(499 * 100);
+
+// 4. Write a function named 'processTeaOrder' that takes another function 'makeTea', as a parameter and calls it with the argument "earl grey". Return the result of calling 'makeTea'.
+
+function makeTea(typeOfTea) {
+  return `makeTea: ${typeOfTea}`;
+}
+
+function processTeaOrder(teaFunction) {
+  return teaFunction("earl grey");
+}
+
+let order = processTeaOrder(makeTea);
+// console.log(order);
+
+// 5. Write a function named 'createTeaMaker' that returns another function. The returned function should take one parameter, 'teaType', and return a message like "Making gree tea". Store the returned function in a variable named 'teaMaker' and call it with "green tea".
+
+function createTeaMaker() {
+  return function (teaType) {
+    return `Making ${teaType}`;
+  };
+}
+
+let teaMaker = createTeaMaker();
+// console.log(teaMaker("Black Tea"));
